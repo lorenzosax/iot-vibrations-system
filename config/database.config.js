e42ca473af6dee5995faa21d9e6239e6a1afb2f1
Lorenzo Gagliani <lorenz_g@hotmail.it>
2020-06-09 18:30:43 +0200

    test db connection to mongodb.com

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/easy-notes'
+    url: 'mongodb://admin:adminpwd@cluster0-shard-00-00-ntyou.mongodb.net:27017,cluster0-shard-00-01-ntyou.mongodb.net:27017,cluster0-shard-00-02-ntyou.mongodb.net:27017/mr2020?'
 }
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var queryString = require('query-string');
+
+var opts = {
+    ssl: true,
+    replicaSet: 'Cluster0-shard-0',
+    authSource: 'admin',
+    retryWrites: true,
+};
+const options = queryString.stringify(opts);
 
 // create express app
 var app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Configuring the database
 var dbConfig = require('./config/database.config.js');
@@ -16,17 +25,26 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(dbConfig.url, {
-	useMongoClient: true
+mongoose.connect(dbConfig.url+options, {
+    useMongoClient: true
 });
 
 mongoose.connection.on('error', function() {
     console.log('Could not connect to the database. Exiting now...');
     process.exit();
 });
-mongoose.connection.once('open', function() {
+mongoose.connection.once('connected', function() {
     console.log("Successfully connected to the database");
-})
+});
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose default connection is disconnected');
+});
+process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
+        console.log('Mongoose connection is disconnected');
+        process.exit(0);
+    });
+});
 
 // define a simple route
 app.get('/', function(req, res){
