844368fc9a3caa2f05fd3e8c9b799035f895be63
Lorenzo Gagliani <lorenz_g@hotmail.it>
2020-06-12 15:49:02 +0200

    - test chart

--- public/js/sensor-charts.js
+++ public/js/sensor-charts.js
@@ -1,63 +1,69 @@
-let lastDate = 0;
 let dataX = [];
 let dataY = [];
 let dataZ = [];
-let TICKINTERVAL = 86400000;
-let XAXISRANGE = 777600000;
+let DAYRANGE = 86400000;
+let XAXISRANGE = 3600000; // 60 min
 
-function getDayWiseTimeSeries(baseval, count, yrange) {
-	let i = 0;
-	while (i < count) {
-		let x = baseval;
-		let y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) +
-			yrange.min;
-		dataX.push({x, y});
-		y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) +
-			yrange.min;
-		dataY.push({x, y});
-		y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) +
-			yrange.min;
-		dataZ.push({x, y});
-		lastDate = baseval;
-		baseval += TICKINTERVAL;
-		i++;
-	}
+let vibrationChart;
+
+function calculateCorrectMaxAndMinYaxis() {
+	let min = [];
+	let max = [];
+	min.push((_.min(dataX, function(o) {return o.y;})).y);
+	min.push((_.min(dataY, function(o) {return o.y;})).y);
+	min.push((_.min(dataZ, function(o) {return o.y;})).y);
+	max.push((_.max(dataX, function(o) {return o.y;})).y);
+	max.push((_.max(dataY, function(o) {return o.y;})).y);
+	max.push((_.max(dataZ, function(o) {return o.y;})).y);
+	vibrationChart.opts.yaxis[0].min = _.min(min);
+	vibrationChart.opts.yaxis[0].max = _.max(max);
 }
 
-getDayWiseTimeSeries(new Date('11 Feb 2017 GMT').getTime(), 10,
-	{
-		min: 10,
-		max: 90,
-	});
-
-function getNewSeries(baseval, yrange) {
-	let newDate = baseval + TICKINTERVAL;
-	lastDate = newDate;
-
-	for (let i = 0; i< dataX.length - 10; i++) {
-		// IMPORTANT
-		// we reset the x and y of the data which is out of drawing area
-		// to prevent memory leaks
-		dataX[i].x = newDate - XAXISRANGE - TICKINTERVAL;
-		dataX[i].y = 0;
-		dataY[i].x = newDate - XAXISRANGE - TICKINTERVAL;
-		dataY[i].y = 0;
-		dataZ[i].x = newDate - XAXISRANGE - TICKINTERVAL;
-		dataZ[i].y = 0;
+function prepopulateVibrationChart(count, data) {
+	let i = 0;
+	let now = Date.now();
+	while (i < count) {
+		let x = new Date(data[i].createdAt).getTime();
+		if (now - x <= XAXISRANGE) {
+			dataX.push({x, y:data[i].axes[0]});
+			dataY.push({x, y:data[i].axes[1]});
+			dataZ.push({x, y:data[i].axes[2]});
+		}
+		i++;
 	}
+	calculateCorrectMaxAndMinYaxis();
+}
+
+function putNewVibrationData(data) {
+	/*
+	let now = Date.now();
+	for (let i = 0; i < dataX.length; i++) {
+		// IMPORTANT
+		// remove data which is out of drawing area
+		// to prevent memory leaks
+		if (now - dataX[i].x > XAXISRANGE) {
+			dataX.shift();
+			dataY.shift();
+			dataZ.shift();
+		} else {
+			break;
+		}
+	}
+	*/
 
 	dataX.push({
-		x: newDate,
-		y: Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min,
+		x: new Date(data.createdAt).getTime(),
+		y: data.axes[0],
 	});
 	dataY.push({
-		x: newDate,
-		y: Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min,
+		x: new Date(data.createdAt).getTime(),
+		y: data.axes[1],
 	});
 	dataZ.push({
-		x: newDate,
-		y: Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min,
+		x: new Date(data.createdAt).getTime(),
+		y: data.axes[2],
 	});
+	calculateCorrectMaxAndMinYaxis();
 }
 
 /*
@@ -124,29 +130,30 @@ $(document).ready(function() {
 		},
 	};
 
-	let vibrationChart =
+	vibrationChart =
 		new ApexCharts(document.querySelector('#vibration-chart'), options);
-	vibrationChart.render();
 
-	window.setInterval(function() {
-		getNewSeries(lastDate, {
-			min: 10,
-			max: 90,
-		});
-
-		vibrationChart.updateSeries([{
-			data: dataX,
+	let numInitialData = 30;
+	$.ajax({
+		url: '/vibrations?last='+numInitialData,
+		success: function(data) {
+			prepopulateVibrationChart(
+				numInitialData,
+				data);
+			vibrationChart.render();
+			let socket = io();
+			socket.on('sensor', function(data) {
+				putNewVibrationData(data);
+				vibrationChart.updateSeries([{
+					data: dataX,
+				},
+				{
+					data: dataY,
+				},
+				{
+					data: dataZ,
+				}]);
+			});
 		},
-		{
-			data: dataY,
-		},
-		{
-			data: dataZ,
-		}]);
-	}, 1000);
-
-	let socket = io();
-	socket.on('sensor', function(obj) {
-		console.log(obj);
 	});
 });
